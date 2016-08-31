735095954deef19b464d87403c91b07408f9e96c
Christer Jonassen <christer@csdnserver.com>
2016-08-31 15:00:06 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # hoarder
 YouTube archiving script based on youtube-dl
+Suitable for manual and automated YouTube video hoarding.
+
+It's basically a youtube-dl launcher with predefined options
+and an optional glorified while loop featuring an interactive countdown.
+
+Requires [youtube-dl](http://rg3.github.io/youtube-dl/).
+
+# Configuration
+
+Hoarder needs a directory to both store configuration and download content in.
+
+The `$hoarder_data_dir` can be read from the environment,
+or sourced from a `hoarder.cfg` in the current directory.
+
+The `$hoarder_data_dir` needs to contain a textfile called `hoarder_urls.txt`
+containing the URLs you want to hoard.
+
+Options for hoarder mode and horder wait interval
+can be read from the environment, `$hoarder_mode`, `$hoarder_wait`,
+or sourced from a `hoarder.cfg` in the current directory,
+or specified as positional parameters (`$1` is mode, `$2` is interval):
+`./hoard.sh loop 10`
+
+If no mode is specified, `hoard.sh` defaults to run-once mode.
+
+# How it works
+
+When you start `hoard.sh`, it checks if `$hoarder_data_dir` is set
+and if it exists. It also sets defaults for non-critical options that aren't set
+
+It then starts `youtube-dl` with some hard-coded options.
+Files are downloaded to `$hoarder_data_dir/<uploader name>/`
+They are named `<uploader-name>-<video tittle>.<file extension>`
+Output container is `.mkv` and subs are downloaded and included where applicable
+youtube-dl saves the IDs to `$hoarder_data_dir/hoarder_archive.txt`,
+so that already downloaded content will be skipped next time.
+
+If looping mode is activated, hoard.sh waits the specified interval before
+launching `youtube-dl` again
+
+So all you need to do is to add YouTube video/channel/user/playlist URLs and
+hoarder will make sure to hoard all those nice videos for you.
---
+++ hoard.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# hoard.sh
+#
+# Basically youtube-dl with hard coded options
+# with an optional glorified while loop featuring an interactive countdown.
+#
+# By default it runs youtube-dl once,
+# otherwise specify looping and interval between loops
+# e.g "./hoard.sh loop 10"
+#
+trexit(){ #exit for traps, trexit!
+  echo "hoarder terminated at $(date)"
+  tput cnorm
+  tput sgr0
+  exit
+}
+
+mexit(){ #exit with message, mexit!
+  echo "FATAL: $1" && exit
+}
+
+init() {
+  trap trexit SIGINT SIGTERM
+  if [[ -z "$hoarder_data_dir" ]]; then
+    if [[ -f "hoarder.cfg" ]]; then
+       source hoarder.cfg
+       [[ -z "$hoarder_data_dir" ]] && mexit "hoarder_data_dir is not set in hoarder.cfg"
+    fi
+    [[ -z "$hoarder_data_dir" ]] && mexit "hoarder_data_dir is not set and hoarder.cfg could not be located."
+  fi
+
+  [[ -d "$hoarder_data_dir" ]] || mexit "hoarder_data_dir '$hoarder_data_dir' does not exist."
+  [[ -f "$hoarder_data_dir/hoarder_urls.txt" ]] || mexit "$hoarder_data_dir/hoarder_urls.txt does not exist. Please create it and populate it with the URLs you wish to hoard from."
+  [[ -f "$hoarder_data_dir/hoarder_archive.txt" ]] || echo "NOTICE: $hoarder_data_dir/hoarder_archive.txt does not exist, a new one will be created."
+
+  if [[ "$hoarder_mode" == "loop" ]]; then
+    if [[ -z "$hoarder_wait" ]]; then
+      [[ -z "$2" ]] && hoarder_wait=43200 && echo "hoarder_wait was not set, defaulting to $hoarder_wait." || hoarder_wait="$2"
+    fi
+  fi
+}
+
+hoard(){
+  echo "Hoarding started at $(date)"
+  youtube-dl \
+  --batch-file "$hoarder_data_dir/hoarder_urls.txt" \
+  --download-archive "$hoarder_data_dir/hoarder_archive.txt" \
+  --embed-subs \
+  --restrict-filenames \
+  --all-subs \
+  --merge-output-format mkv \
+  -o "$hoarder_data_dir/%(uploader)s/%(uploader)s-%(title)s.%(ext)s"
+  echo "Hoarding ended at $(date)"
+}
+
+waitcounter(){
+  echo -n "Next hoarding countdown: "
+  tput sc
+  tput civis
+  hoarder_wait_countdown="$hoarder_wait"
+  while [[ "$hoarder_wait_countdown" -ne 0 ]]; do
+    tput rc
+    tput el
+    echo -n "$hoarder_wait_countdown"
+    sleep 1
+    (( hoarder_wait_countdown-- ))
+  done
+  tput cnorm
+  echo
+}
+
+hoarder(){
+  if [[ "$hoarder_mode" == "loop" ]]; then
+      while true; do
+        hoard
+        waitcounter
+      done
+  else
+      hoard
+ fi
+}
+
+init "$1" "$2"
+hoarder
